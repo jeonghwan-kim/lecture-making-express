5b0a368178b49a94b3fa4c6a8191a2151a3bc643
김정환 <ej88ej@gmail.com>
2017-11-01 11:27:00 +0900

    debug 모듈 색상추가

--- modules/debug.js
+++ modules/debug.js
@@ -1,9 +1,24 @@
-const debugEnabled = process.env.DEBUG || false
+const colors = [
+  {name: 'cyan',     value: '\x1b[36m'},
+  {name: 'yellow',   value: '\x1b[33m'},
+  {name: 'red',      value: '\x1b[31m'},
+  {name: 'green',    value: '\x1b[32m'},
+  {name: 'magenta',  value: '\x1b[35m'},
+]
+const resetColor = '\x1b[0m'
 
 const debug = tag => {
-  return msg => {
-    tag ? console.log(tag, msg) : console.log(msg)
+  const dbg = (...msg) => {
+    console.log(dbg.coloredTag, ...msg)
   }
+
+  dbg.coloredTag = (()=> {
+    const randIdx = Math.floor(Math.random() * colors.length) % colors.length
+    const color = colors[randIdx]
+    return `${color.value}[${tag}]${resetColor}`
+  })()
+
+  return dbg
 }
 
 module.exports = debug
--- modules/debug.spec.js
+++ modules/debug.spec.js
@@ -1,43 +1,37 @@
-const assert = require('assert')
-const debug = require('./debug')('tag')
-
-const spy = {
-  on (obj, method) {
-    const originalFn = obj[method]
-    this.method = method
-    this.haveBeenCalled = false
-
-    obj[method] = () => {
-      this.haveBeenCalled = true
-      obj[method] = originalFn
-    }
-  },
-  expect(target) {
-    this.target = target
-    return this
-  },
-  toHaveBeenCalled () {
-    if (!this.haveBeenCalled) throw Error(`${this.method}() 호출되지 않았음`)
-  }
-}
+const assert = require('assert');
+const sinon = require('sinon');
+const debug = require('./debug')
 
 describe('debug 모듈', () => {
+  let spy
+
+  beforeEach(()=> {
+    process.env.DEBUG = '*'
+    spy = sinon.spy(console, 'log')
+  })
+
+  afterEach(()=> {
+    delete process.env.DEBUG
+    console.log.restore()
+  })
+
   it('함수를 반환한다', () => {
-    assert.equal(typeof debug, 'function')
+    assert.equal(typeof debug(), 'function')
   })
 
   it('반환한 함수는 console.log()를 실행한다', () => {
-    spy.on(console, 'log')
-    debug()
-    spy.expect(console.log).toHaveBeenCalled()
+    const dbg = debug()
+    dbg()
+    assert.equal(spy.called, true)
+    
   })
 
-  // it('설정한 태그로 로그를 기록한다', () => {
-  //   const tag = 'tag 1'
-  //   const msg = 'msg 1'
-  //   const debug = require('./debug')(tag)
-  //   spy.on(console, 'log')
-  //   debug(msg)
-  //   spy.Expect()toHaveBeenCalledWith(console.log)
-  // })
+  it('설정한 태그와 메세지로 로그를 출력한다', () => {
+    const tag = 'tag 1'
+    const msg = 'msg 1'
+    const dbg = debug(tag)
+    dbg(msg)
+    assert.equal(spy.calledWith(dbg.coloredTag, msg), true)
+    assert.equal(dbg.coloredTag.includes(tag), true)
+  })
 })
