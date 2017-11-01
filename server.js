391650bee09c11249112b34e4f9d610078d5ef65
김정환 <ej88ej@gmail.com>
2017-11-01 14:30:17 +0900

    정적파일 제공 기능 구현

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const debug = require('./modules/debug')('server')
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.eot': 'appliaction/vnd.ms-fontobject',
+  '.ttf': 'aplication/font-sfnt'
+  };
+  const ext = path.parse(req.url).ext;
+  const publicPath = path.join(__dirname, './public')
+
+  debug('ext:', ext)
+
+  if (Object.keys(mimeType).includes(ext)) {
+    fs.readFile(`${publicPath}${req.url}`, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        return res.end('Not found');    
+      }
+
+      res.statusCode = 200
+      res.setHeader('Content-Type', mimeType[ext]);
+      res.end(data)
+    })
+    return 
+  } 
+    
+  res.statusCode = 404;
+  res.end('Not found');
 });
 
 module.exports = server
