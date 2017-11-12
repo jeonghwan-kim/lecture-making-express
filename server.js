804f8bd36f4f54bf1d47fe1892ef7b45297f4e5d
Kim Jeonghwan <ej88ej@gmail.com>
2017-11-12 17:08:11 +0900

    / 라우팅 설정

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const serveStatic = require('./middlewares/serve-static')
 const helloWorld = require('./middlewares/hello-world')
 const morgan = require('./middlewares/morgan')
 
+// routes
+const routes = require('./routes/index')
+
 // app
 const app = Application()
 
 app.use(morgan)
 app.use(serveStatic)
-app.use(helloWorld)
+app.use('/', routes)
 
 module.exports = app.server
--- server.spec.js
+++ server.spec.js
@@ -2,8 +2,23 @@ const assert = require('assert')
 const request = require('supertest')
 const server = require('./server')
 
-describe('server', () => {
-  it('hello world를 응답한다', done => {
+describe('/', () => {
+  it.only('index.html을 응답한다', done => {
+    request(server)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect(res => {
+        assert.equal(res.text.includes('<html>'), true)
+        assert.equal(res.text.includes('<body>'), true)
+        assert.equal(res.text.includes('</body>'), true)
+        assert.equal(res.text.includes('</html>'), true)
+        
+      })
+      .end(done)
+  })
+
+  it.skip('hello world를 응답한다', done => {
     request(server)
       .get('/')
       .expect(200)
