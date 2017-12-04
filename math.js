6ea77c49b75876fb67282534b535400ce83f05cd
김정환 <ej88ej@gmail.com>
2017-12-04 21:04:27 +0900

    mul() 함수를 위한 테스트 케이스

--- math.js
+++ math.js
@@ -4,12 +4,15 @@ const toNum = (val) => {
   return num || 0
 }
 
-const sum = (a, b) => toNum(a) + toNum(b);
-
-const substract = (a, b) => toNum(a) - toNum(b);
+const math = {
+  sum(a, b) {
+    return toNum(a) + toNum(b);
+  }
+}
 
 const messages = {
   notNumber: '인자가 Number이거나 숫자형식의 String이어야 함'
 }
 
-module.exports = {sum, substract, messages}
+module.exports = math;
+module.exports.messages = messages;
