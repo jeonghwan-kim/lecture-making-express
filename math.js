bd069a25fcf1e2621593443e0b0fd366a6f48a52
Kim Jeonghwan <ej88ej@gmail.com>
2017-12-04 20:54:11 +0900

    sum() 함수 예외 사항 처리를 위한 테스트 케이스

--- math.js
+++ math.js
@@ -1,15 +1,8 @@
-const toNum = (val) => {
-  const num = (typeof val === 'string') ? parseInt(val, 10) : val;
-  if (Number.isNaN(num)) throw Error(messages.notNumber)
-  return num || 0
+const math = {
+  sum(a, b) {
+    return a + b;
+  }
 }
 
-const sum = (a, b) => toNum(a) + toNum(b);
 
-const substract = (a, b) => toNum(a) - toNum(b);
-
-const messages = {
-  notNumber: '인자가 Number이거나 숫자형식의 String이어야 함'
-}
-
-module.exports = {sum, substract, messages}
+module.exports = math
--- math.spec.js
+++ math.spec.js
@@ -4,64 +4,19 @@ const assert = require('assert');
 describe('math module', () => {
   describe('sum()', ()=> {
     it('두 수를 더한 값을 반환한다', () => {
-      assert.equal(math.sum(1,2), 3)
+      assert.equal(math.sum(1, 2), 3)
     });
 
     it('문자열 정수를 더할 수 있다', () => {
-      assert.equal(math.sum(1, '2'), 3)
-      assert.equal(math.sum('1', '2'), 3)
-      assert.equal(math.sum('1', 2), 3)
+      // todo 
     });
 
     it('정수형식의 문자열이 아닐경우 에러를 던진다', () => {
-      try {
-        math.sum('one', 2)
-      } catch (e) {
-        return assert.equal(e.message, math.messages.notNumber)
-      }
-
-      try {
-        math.sum('1', 'two')
-      } catch (e) {
-        return assert.equal(e.message, math.messages.notNumber)
-      }
-
-      assert.equal(true, false)
+      // todo
     });
 
     it('인자를 1개만 넣은 경우 그 값을 반환한다', () => {
-      assert.equal(math.sum(2), 2)
-    })
-  });
-
-  describe('subtract()', () => {
-    it('두 수를 뺀 값을 반환한다', () => {
-      assert.equal(math.substract(2,1), 1)
-    });
-
-    it('문자열 값을 뺄수 있다', () => {
-      assert.equal(math.substract('2', 1), 1)
-      assert.equal(math.substract('2','1'), 1)
-      assert.equal(math.substract(2,'1'), 1)
-    });
-
-    it('정수형식의 문자열이 아닐경우 에러를 던진다', () => {
-      try {
-        math.substract('one', 2)
-      } catch (e) {
-        return assert.equal(e.message, math.messages.notNumber)
-      }
-
-      try {
-        math.substract(1, 'two')
-      } catch (e) {
-        return assert.equal(e.message, math.messages.notNumber)
-      }
-      assert.equal(true, false)
-    });
-
-    it('인자를 1개만 넣은 경우 그 값을 반환한다', () => {
-      assert.equal(math.substract(2), 2)
+      // todo 
     })
   });
 });
